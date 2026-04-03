c2df7621dd3daab42294e95205b67dc8333d59d6
pravez <paul.bretonravez@proton.me>
2026-04-03 10:36:27 +0200

    ci: add auto deploy through ssh

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import { createMDX } from 'fumadocs-mdx/next';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,      
+  },
 };
 
 const withMDX = createMDX();
